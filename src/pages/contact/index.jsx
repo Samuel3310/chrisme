b76a87ed98de95c16364144718b979ce81b4152e
samuel3310 <awoluowolabisamuel@gmail.com>
2025-01-01 11:20:39 +0000

    fixed

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -236,8 +236,8 @@ const GoogleMap = () => {
   return (
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126614.513462581!2d3.8226579119554707!3d7.387080141948376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10398d77eeff086f%3A0x3b33e0f76e8e04a9!2sIbadan%2C%20Oyo!5e0!3m2!1sen!2sng!4v1733912713176!5m2!1sen!2sng"
-      width="90%"
-      height="450"
+      width="95%"
+      height="400"
       style={{ border: 0 }}
       allowFullScreen
       loading="lazy"
